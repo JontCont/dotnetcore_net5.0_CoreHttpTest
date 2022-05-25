680b22318bef297f09af1f33ba41450eb69a5a52
jontcont <52921053+JontCont@users.noreply.github.com>
2022-05-25 23:07:14 +0800

    initial

---
+++ CoreHttpTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreHttpTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
